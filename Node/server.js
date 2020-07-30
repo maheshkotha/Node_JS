79fe7d39160b613694ba5a6c6b9b305da258a8ba
Mahesh Kotha <44569697+maheshkotha@users.noreply.github.com>
2020-07-30 16:51:51 +0530

    add two more routes
    logout and signup routes

--- Node/server.js
+++ Node/server.js
@@ -8,6 +8,14 @@ app.get('/', (req, res) => {
     res.send("Hello NODE");
 });
 
+app.get('/logout', (req, res) => {
+    res.send("Your are loged out");
+});
+
+app.get('/signup', (req, res) => {
+    res.send("You are visited sign up route");
+});
+
 app.listen(PORT, () => console.log(`http://localhost:${PORT}`));
 
  
